8e5d59ddc74537072c04f89317d2d46fc7814222
Jason Hansel <jason@jasonhansel.com>
2018-05-12 14:31:53 -0400

    Moving files around

--- zsh/.zsh/start-vim.zsh
+++
@@ -1,8 +0,0 @@
-# Based on the oh-my-zsh plugin, whose autostart feature
-# doesn't work for me. Specifically, the prompt doesn't
-# work after exiting tmux if I use the plugin.
-
-if [[ ! -n "$VIMRUNTIME" ]] 
-then
-	# DTACH=1 dtach -A ~/.dtach -e ^P vim -c "term ++curwin"
-fi
