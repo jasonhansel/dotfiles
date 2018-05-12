d73a750ea44b684a2636c8b45510429971c334f6
Jason Hansel <jason@jasonhansel.com>
2018-05-12 14:32:54 -0400

    Moving more things

---
+++ global/.zsh/start-tmux.zsh
@@ -0,0 +1,16 @@
+# Based on the oh-my-zsh plugin, whose autostart feature
+# doesn't work for me. Specifically, the prompt doesn't
+# work after exiting tmux if I use the plugin.
+
+if [[ ! -n "$TMUX" ]] 
+then 
+	if [[ "$ZSH_TMUX_AUTOSTARTED" != "true" ]] 
+	then 
+		export ZSH_TMUX_AUTOSTARTED=true 
+		tmux attach || tmux new-session
+		if [[ -n "$SSH_CONNECTION" ]] 
+		then
+			exit
+		fi
+	fi
+fi
