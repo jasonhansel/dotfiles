932a72f88104afa227d3c923e0ad4227931f5aeb
Jason Hansel <jason@jasonhansel.com>
2018-05-16 21:43:47 -0400

    Misc work

--- global/.config/sublime-text-3/Packages/User/expand.py
+++ global/.config/sublime-text-3/Packages/User/expand.py
@@ -9,31 +9,48 @@ import unittest
 import itertools
 import collections
 
+brackets = {
+    '' : '',
+    '(' : ')',
+    '{' : '}',
+    '﹝' : '﹞'
+}
+inv_brackets = {
+    v : k
+    for k, v in brackets.items()
+}
+
 
 def parse(text):
     level = ''
     ret = []
     thus_far = ''
+    first = True
     for char in text:
         to_append = char
         if level == '':
-            if char == '(':
-                if len(thus_far.strip()): ret.append([thus_far.strip()])
+            if char == '(' or char == '﹝':
+                if len(thus_far.strip()): ret.append([('', thus_far.strip())])
                 thus_far = ''
                 ret.append([])
                 to_append = ''
-        if level == '(':
-            if (char == '+' or char == '-' or char == ')'):
-                if len(thus_far.strip()):  ret[-1].append(thus_far.strip())
+
+        if level == '(' or level == '﹝':
+            if (char == '+' or char == '-' or char == brackets[level]):
+                if len(thus_far.strip()): ret[-1].append((level[-1], thus_far.strip()))
                 thus_far = ''
-            if char == ')':
+            if char == brackets[level]:
                 to_append = ''
-        if char == '(': level += '(';
-        elif char == ')' and level[-1] == '(': level = level[:-1]
-        if char == '{': level += '{';
-        elif char == '}' and level[-1] == '{': level = level[:-1]
+
+        if char in brackets:
+            level += char
+        elif char in inv_brackets and inv_brackets[char] == level[-1]:
+            level = level[:-1]
+
         thus_far += to_append
-    if len(thus_far.strip()): ret.append([thus_far.strip()])
+        first = False
+
+    if len(thus_far.strip()): ret.append([('', thus_far.strip())])
     thus_far = ''
     return ret
 
@@ -43,7 +60,7 @@ def expand(text):
     for combo in combos:
         out = ''
         mul = 1
-        for part in combo:
+        for (bracket, part) in combo:
             if part[0] == '+':
                 mul *= 1
                 part = part[1:]
@@ -55,7 +72,7 @@ def expand(text):
                 mul *= int(part)
                 part = ''
             if len(part):
-                out += '(' + part.strip() + ')'  
+                out += bracket + part.strip() + brackets[bracket]
         if out in ret:
             ret[out] += mul
         else:
@@ -72,19 +89,6 @@ def expand(text):
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_parse(self):
-        self.assertEqual(
-            parse("22(50)"),
-            [["22"], ["50"]]
-        )
-        self.assertEqual(
-            parse("22(x + y(a + b) - z)4"),
-            [["22"], ["x", "+ y(a + b)", "- z"], ["4"]]
-        )
-        self.assertEqual(
-            parse("(x - 5)22(x + y(a + b) - z)4"),
-            [["x", "- 5"], ["22"], ["x", "+ y(a + b)", "- z"], ["4"]]
-        )
     def test_expand(self):
         self.assertEqual(
             expand("22(50)"),
@@ -102,10 +106,43 @@ class TestStringMethods(unittest.TestCase):
             expand("(x - 5)(x - 5)"),
             "+ (x)(x)\n- 10(x)\n+ 25"
         )
+        self.assertEqual(
+            expand("z(x - 5)(x - 5)"),
+            "+ z(x)(x)\n- 10z(x)\n+ 25z"
+        )
+        self.assertEqual(
+            expand("z﹝x - 5﹞(x - 5)"),
+            "+ z﹝x﹞(x)\n- 5z﹝x﹞\n- 5z(x)\n+ 25z"
+        )
+    def test_div(self):
+        self.assertEqual(
+            expand("{x5 - x10(z + 1) ／ 3}"),
+            "+ {x5 ／ 3} - {x10(z + 1) ／ 3}"
+        )
+        self.assertEqual(
+            expand("{x}\sin{x5 - x10(z + 1) ／ 3}"),
+            "+ {x}\sin{x5 - x10(z + 1) ／ 3}"
+        )
+        self.assertEqual(
+            expand("﹝x5 - x10 ／ 3﹞"),
+            "﹝+ {x5 ／ 3} - {x10 ／ 3}﹞"
+        )
+        self.assertEqual(
+            expand("{x5 - x10 ／ 3}(x + 1)"),
+            "+ {x5 - x10 ／ 3}(x) + {x5 - x10 ／ 3}(1)"
+        )
+        self.assertEqual(
+            expand("{x5 - x10 ／ 3}﹝x + 1﹞"),
+            "+ {x5 - x10 ／ 3}﹝x﹞ + {x5 - x10 ／ 3}﹝1﹞"
+        )
+        self.assertEqual(
+            expand("﹝x5 - x10 ／ 3﹞﹝x + 1﹞"),
+            "+ ﹝x5 - x10 ／ 3﹞﹝x﹞ + ﹝x5 - x10 ／ 3﹞﹝1﹞"
+        )
     def test_fix(self):
         self.assertEqual(
             expand("x^{(5-3)}"),
-            "+ (x^{(5-3)})"
+            "+ x^{(5-3)}"
         )
 
 if __name__ == '__main__':
@@ -121,3 +158,17 @@ class JasonExpand(sublime_plugin.TextCommand):
             self.view.replace(edit, selection[0], expand(self.view.substr(selection[0])))
         print(self.view)
         # self.view.insert(edit, 0, "Hello, World!")
+
+class JasonVars(sublime_plugin.TextCommand):
+    def run(self, edit):
+        selection = self.view.sel()
+        numbers = []
+        def repl(match):
+            greek = match.group(0)
+            if greek not in numbers:
+                numbers.append(greek)
+            return "${" + str(numbers.index(greek) + 1) + ":" + greek + "}"
+
+        if len(selection) == 1:
+            text = self.view.substr(selection[0])
+            self.view.run_command("insert_snippet", {"contents": re.sub('[Α-ω]', repl, text) } )
